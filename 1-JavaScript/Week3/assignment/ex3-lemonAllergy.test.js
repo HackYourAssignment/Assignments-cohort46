59e168234d907587ac25f2012ebc5af1afe13244
burhan1997 <burhanelaldi97@gmail.com>
2023-11-20 11:21:58 +0100

    ex 3 solved

--- 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
@@ -26,26 +26,34 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, unwantedFruit) {
+  return fruitBasket.filter((fruit) => fruit !== unwantedFruit);
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket.length).toBe(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    // Call the function
+    sanitizeFruitBasket(fruitBasket, 'lemon');
+
+    // Check if the original array is unchanged
+    expect(fruitBasket).toEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    // Call the function
+    const sanitizedBasket = sanitizeFruitBasket(fruitBasket, 'lemon');
+
+    // Check if the new array does not include 'lemon'
+    expect(sanitizedBasket).not.toContain('lemon');
   });
 });
