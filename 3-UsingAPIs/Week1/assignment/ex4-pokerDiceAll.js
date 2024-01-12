30812e2f94cb7a77e058a838f1c396b01753b9d9
Ali Othman <alioth840@gmail.com>
2024-01-12 22:53:23 +0100

    I have implemented the feedback

--- 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
@@ -1,47 +1,23 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/3-UsingAPIs/Week1#exercise-4-throw-the-dice-for-a-poker-dice-game
-
-For this exercise you should do the following:
-  - Refactor the `rollDice()` function to throw five dice in one go, by 
-    using `.map()` on the `dice` array to create an array of promises for use 
-    with `Promise.all()`.
-  - A successful (i.e. resolved) throw should output a message similar to: 
-      Resolved! [ 'JACK', 'QUEEN', 'QUEEN', 'NINE', 'JACK' ]
-  - An unsuccessful (i.e. rejected) throw should output a message similar to:
-      Rejected! Die 3 rolled off the table.
-
-The provided rollDie() function logs the value of a die as it rolls, 
-time-stamped with the time of day (with millisecond accuracy) to the console. 
-Once you have successfully completed this exercise you will notice that the 
-intermediate messages are output in bursts of up to five at a time as the dice 
-finish rolling asynchronously.
-
-You may also notice that, in the case of a rejected promise, dice that have not
-yet finished their roll continue to do so. 
-Can you explain why? Please add your answer as a comment to the end of the 
-exercise file.
-------------------------------------------------------------------------------*/
-
-// The line below makes the rollDie() function available to this file.
-// Do not change or remove it.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
+
   const dice = [1, 2, 3, 4, 5];
 
-  function rollDicePromise(eachDice) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(`${eachDice}`);
-        }, 1000); // Simulating a delay of 1000 milliseconds (1 second)
-    });
-  };
-
-  const arrayOfRollDicePromises = dice.map(rollDicePromise);
+  const arrayOfRollDicePromises = dice.map(eachDice =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() < 0.5) {
+          resolve(`${eachDice}`);
+        } else {
+          reject(new Error(`Die ${eachDice} rolled off the table.`));
+        }
+      }, 1000);
+    })
+  );
 
   return Promise.all(arrayOfRollDicePromises);
-};
+}
 
 function main() {
   rollDice()
@@ -53,11 +29,11 @@ function main() {
       });
 }
 
-// ! Do not change or remove the code below
+
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
-module.exports = rollDice;
+module.exports = rollDie;
 
 /*
     The resaen that rejected promise allows the athour resolved dice that have not
